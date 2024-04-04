c9353619771ce628fe08d1cba8ad06630cf71c8f
buptLiYangCode <1642612105@qq.com>
2024-04-04 18:49:45 +0800

    空调调度员功能完善

--- bupt-hotel-2/src/main/java/com/example/controller/DispatcherController.java
+++ bupt-hotel-2/src/main/java/com/example/controller/DispatcherController.java
@@ -1,10 +1,63 @@
 package com.example.controller;
 
+import com.example.common.result.Result;
+import com.example.common.result.Results;
+import com.example.dto.req.AdminStartReqDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+/**
+ * 空调资源有限，同时只有部分空调处分，调度员负责调度房间空调出风
+ */
 @RestController
 @RequiredArgsConstructor
 public class DispatcherController {
+
+    /**
+     * 查询空闲资源个数
+     */
+    @GetMapping("/dispatcher/query")
+    public Result<Integer> queryFreeResources() {
+
+
+        return Results.success(null);
+    }
+
+    /**
+     * 当前有k个资源，查询等待列表前k个空调编号
+     */
+    @GetMapping("/dispatcher/unworkable")
+    public Result<List<String>> queryTopKWaitingACNumbers(Integer k) {
+
+        return Results.success(null);
+    }
+
+    /**
+     * 给这k个空调分配资源
+     */
+    @PostMapping("/dispatcher/establish")
+    public Result<Void> establishConnection(List<String> acNumberList) {
+
+        return Results.success(null);
+    }
+
+    /**
+     * 检查服务队列中空调是否有用尽时间片的
+     */
+    @GetMapping("/dispatcher/check-unworkable")
+    public Result<List<String>> checkUnworkableACs() {
+        return Results.success(null);
+    }
+
+    /**
+     * 释放时间片用完的空调资源
+     */
+    @PostMapping("/dispatcher/release")
+    public Result<Void> releaseResources(List<String> acNumberList) {
+
+        return Results.success(null);
+    }
 
 }
